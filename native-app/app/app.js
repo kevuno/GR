8b34ac95d84e4345828fedf709f718b10c825c5b
Kevin Bastian <Kevin@homes-MacBook-Pro.local>
2020-01-21 22:34:58 -0500

    Adding initial app

---
+++ native-app/app/app.js
@@ -0,0 +1,12 @@
+import Vue from "nativescript-vue";
+
+import App from "./components/App";
+
+import VueDevtools from 'nativescript-vue-devtools'
+
+Vue.use(VueDevtools)
+
+new Vue({
+    render: h => h(App)
+}).$start();
+

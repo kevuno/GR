659bffa6367f9fc37cb198433d650e5ced7e47bb
Kevin Bastian <Kevin@homes-MacBook-Pro.local>
2020-02-16 23:52:49 -0500

    Adding a basic Operation Details page

---
+++ GR/app/components/OperationDetails.vue
@@ -0,0 +1,42 @@
+<template>
+    <Page>
+        <GridLayout rows="auto, *">
+            <Label row="0" class="header" text="Detalles de la Operacion" />
+
+            <GridLayout row="1" columns="40*, 40*, 7*, 13*" rows="auto, auto">
+                <Label row="0" col="0" rowSpan="2" :text="timestampToDateString(operation.timestamp)" />
+                <Label row="0" col="1" class="tanque_title" :text="operation.operation_type.toUpperCase()"></Label>
+                <Label row="1" col="1" :text="operation.operador"></Label>
+                <Label row="0" col="2" rowSpan="2" v-if="operation.operation_type == 'carga'" text="-" fontWeight="Bold" class="h2" />
+                <Label row="0" col="2" rowSpan="2" v-else text="+" fontWeight="Bold" class="h2" />
+                <Label row="0" col="3" rowSpan="2" :text="operation.total_litros_cargados_from_tanques" />
+            </GridLayout>
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+export default {
+    props: ["context"],
+
+    computed: {
+        operation() {
+            return this.context || {};
+        }
+    },
+    methods: {
+        timestampToDateString(timestamp){
+            return new Date(timestamp).toDateString();
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss">
+// Start custom common variables
+@import "~@nativescript/theme/scss/variables/blue";
+// End custom common variables
+
+// Custom styles
+
+</style>
--- GR/app/components/Reporte.vue
+++ GR/app/components/Reporte.vue
@@ -3,24 +3,24 @@
         <GridLayout rows="auto, *">
             <Label row="0" class="header" text="Reporte de Operaciones" />
             <ListView row="1" for="operation in operations" @itemTap="onOperationItemTap">
-            <v-template>
-                <GridLayout columns="40*, 40*, 7*, 13*" rows="auto, auto">
-                    <Label row="0" col="0" rowSpan="2" :text="timestampToDateString(operation.timestamp)" />
-                    <Label row="0" col="1" class="tanque_title" :text="operation.operation_type.toUpperCase()"></Label>
-                    <Label row="1" col="1" :text="operation.operador"></Label>
-                    <Label row="0" col="2" rowSpan="2" v-if="operation.operation_type == 'carga'" text="-" fontWeight="Bold" class="h2" />
-                    <Label row="0" col="2" rowSpan="2" v-else text="+" fontWeight="Bold" class="h2" />
-                    <Label row="0" col="3" rowSpan="2" :text="operation.total_litros_cargados_from_tanques" />
-                </GridLayout>
-
-            </v-template>
-        </ListView>
+                <v-template>
+                    <GridLayout columns="40*, 40*, 7*, 13*" rows="auto, auto">
+                        <Label row="0" col="0" rowSpan="2" :text="timestampToDateString(operation.timestamp)" />
+                        <Label row="0" col="1" class="tanque_title" :text="operation.operation_type.toUpperCase()"></Label>
+                        <Label row="1" col="1" :text="operation.operador"></Label>
+                        <Label row="0" col="2" rowSpan="2" v-if="operation.operation_type == 'carga'" text="-" fontWeight="Bold" class="h2" />
+                        <Label row="0" col="2" rowSpan="2" v-else text="+" fontWeight="Bold" class="h2" />
+                        <Label row="0" col="3" rowSpan="2" :text="operation.total_litros_cargados_from_tanques" />
+                    </GridLayout>
+                </v-template>
+            </ListView>
         </GridLayout>
     </Page>
 </template>
 
 <script>
 import firebase from 'nativescript-plugin-firebase';
+import OperationDetails from './OperationDetails.vue';
 
 export default {
     props: ["context"],
@@ -54,6 +54,18 @@ export default {
     methods: {
         onOperationItemTap(operation){
             console.log("Tapped on operation: " + operation);
+            console.dir(operation.item);
+            this.$showModal(OperationDetails, {
+                transition: {
+                    name:'fade',
+                    duration: 300
+                },
+                props: {
+                    context: operation.item
+                }
+            }).then(msg => console.log(msg)).catch(error => console.log(error));
+            console.log("Modal oppened? " + operation);
+
         },
 
         timestampToDateString(timestamp){

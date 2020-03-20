e093580248f79e0359bac9a8f4a3e9d82abc0fa5
Kevin Bastian <kjb5642@rit.edu>
2020-03-20 02:26:07 +0100

    Editing fixed prices are now saved to firebase db. This was the last feature to implement./lineMain We are done here, good job everyone

--- GR/app/components/EditFixedCosts.vue
+++ GR/app/components/EditFixedCosts.vue
@@ -7,13 +7,13 @@
             <Label class="header" text="Modificar Costos Fijos" />
             <GridLayout horizontalAlignment="center" columns="*, auto" rows="auto, auto, auto, auto">
                 <Label row="0" col="0" text="Precio del Dolar en Pesos: " class="h3"  fontWeight="Bold" />
-                <TextField row="0" col="1" class="h3" v-model="fixed_costs.dollar_to_pesos_rate" width="50" keyboardType="number"/>
+                <TextField row="0" col="1" class="h3" v-model="fixed_costs.dollar_to_pesos_rate" width="50" keyboardType="number" @textChange="onCostChanged()"/>
                 <Label row="1" col="0" text="Costo de 1 Galon de Gasolina (USD): " class="h3" fontWeight="Bold" />
-                <TextField row="1" col="1" class="h3" v-model="fixed_costs.gallon_price_in_dollars" width="50" keyboardType="number" />
+                <TextField row="1" col="1" class="h3" v-model="fixed_costs.gallon_price_in_dollars" width="50" keyboardType="number" @textChange="onCostChanged()"/>
                 <Label row="2" col="0" text="Costo de la aduana por litro (MXN) " class="h3" fontWeight="Bold" />
-                <TextField row="2" col="1" class="h3" v-model="fixed_costs.aduana_cost_in_pesos" width="50" keyboardType="number"/>
+                <TextField row="2" col="1" class="h3" v-model="fixed_costs.aduana_cost_in_pesos" width="50" keyboardType="number" @textChange="onCostChanged()"/>
                 <Label row="3" col="0" text="Utilidad por Litro deseada (MXN)" class="h3" fontWeight="Bold" />
-                <TextField row="3" col="1" class="h3" v-model="fixed_costs.utility_per_liter_in_pesos" width="50" keyboardType="number"/>
+                <TextField row="3" col="1" class="h3" v-model="fixed_costs.utility_per_liter_in_pesos" width="50" keyboardType="number" @textChange="onCostChanged()"/>
             </GridLayout>
         </StackLayout>
     </Page>
@@ -30,22 +30,14 @@ export default {
         },
     },
     methods:{
-        onEstadoFleteChanged(){
-            console.log("Text changed on flete");
-            this.updateDB();
+        onCostChanged(){
+            // Submit changes to Firebase
+            firebase.update(
+                '/precios_fijos',
+                this.fixed_costs
+            );
             
         },
-        onEstadoCostoVariableChanged(){
-            console.log("Text changed Costo variable");
-            this.updateDB();
-        },
-        /**
-         * Calls update on the DB with the given estado object 
-         */
-        updateDB(){
-            // Update DB
-        }
-
     }
 };
 </script>
